dfe7b0cd8d8a4c0f4582bd4c08dd91edaa2ad169
Cecil Andibanbang <andibanbangcecil@gmail.com>
2023-08-17 23:06:06 +0100

    Movie List

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'bookmarks/new'
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+# Defines the root path route ("/")
+
+  resources :lists do
+  resources :bookmarks, only: [:new, :create]
+end
+  resources :bookmarks, only: [:destroy]
 end
